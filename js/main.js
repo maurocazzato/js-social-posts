d44d6ffae59e2193b92ee1221df20d22d29e94a8
Maurocazzato <maurizzio.125@gmail.com>
2023-12-05 16:35:17 +0100

    creazione dei post legati all'array

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 // Milestone 2 - Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 
 // array di oggetti che già trovate.
-const posts = [
+const post = [
     {
         "id": 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
@@ -61,3 +61,47 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+// Ottieni il contenitore dove aggiungerai i div
+const container = document.getElementById('container');
+
+// Funzione per creare un elemento div con header, text, media e footer utilizzando template literals
+function createPostDiv(post) {
+    return `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">${post.author.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${post.content}</div>
+            <div class="post__image">
+                <img src="${post.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`
+      
+};
+
+// Creazione dei div per ogni sezione dell'array e aggiunta al contenitore
+post.forEach(post => {
+    container.innerHTML += createPostDiv(post);
+});
